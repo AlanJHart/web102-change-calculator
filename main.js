5f6da09a30a252c6342f339b82a43d3d3d262166
AlanJHart <hart.alan.j@gmail.com>
2019-03-27 13:49:58 -0700

    Passes all tests, Paid is before Due, handles positive and negative transactions, handles multiple transactions without refreshing page.

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Write your JavaScript here
 let dollar = 100;
 let dollars = 0;
 let quarter = 25;
@@ -10,47 +9,25 @@ let nickels = 0;
 let pennie = 1;
 let pennies = 0;
 
-// document.getElementById("calculate-change").addEventListener("click", handleClickEvent());
-// const button = document.querySelector('button');
-
-
 function handleClickEvent() {
 let due = document.getElementById("amount-due").value;
 let received = document.getElementById("amount-received").value;
-
 let change = 100*(received - due);
-console.log(change);
-dollars = ( change / dollar ) | 0;
-console.log(dollars, change);
+dollars = (change / dollar) | 0;
 change = change % dollar;
-console.log(change);
-quarters = ( change / quarter ) | 0;
-console.log(quarters, change);
+quarters = (change / quarter) | 0;
 change = change % quarter;
-console.log(change);
-dimes = ( change / dime ) | 0;
-console.log(dimes, change);
+dimes = (change / dime) | 0;
 change = change % dime;
-console.log(change);
-nickels = ( change / nickel ) | 0;
-console.log(nickels, change);
+nickels = (change / nickel) | 0;
 change = change % nickel;
-console.log(change);
-pennies = change;
-console.log(pennies, change);
+pennies = Math.round(change);
 
-let output = document.createTextNode( dollars + " DOLLARS " );
-document.getElementById("dollars-output").appendChild(output);
-output = document.createTextNode( quarters +  " QUARTERS ");
-document.getElementById("quarters-output").appendChild(output);
-output = document.createTextNode( dimes + " DIMES ");
-document.getElementById("dimes-output").appendChild(output);
-output = document.createTextNode( nickels + " NICKELS ");
-document.getElementById("nickels-output").appendChild(output);
-output = document.createTextNode( pennies + " PENNIES ");
-document.getElementById("pennies-output").appendChild(output);
-console.log("d" + dollars + "q" + quarters + "d" + dimes + "n" + nickels + "p" + pennies)
+let output;
+    document.getElementById("dollars-output").innerHTML = dollars;
+    document.getElementById("quarters-output").innerHTML = quarters;
+    document.getElementById("dimes-output").innerHTML = dimes;
+    document.getElementById("nickels-output").innerHTML = nickels;
+    document.getElementById("pennies-output").innerHTML = pennies;
 }
-
-let newoutput = document.createTextNode("Change to return to customer is: ");
-    document.getElementById("div1").appendChild(newoutput);
+  
